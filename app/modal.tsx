16813b6df33fd90d779a1d2fa43581d897a8cb50
Bozidar Vujasinovic <bozidar.vujasinovic@scripttic.com>
2024-07-08 19:03:35 +0200

    Initial expo creation app

---
+++ app/modal.tsx
@@ -0,0 +1,31 @@
+import { StatusBar } from 'expo-status-bar';
+import { Platform, StyleSheet } from 'react-native';
+
+import { Text, View } from '@/components/ui/Themed';
+
+export default function ModalScreen() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+      <Text style={styles.title}>Modal</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
