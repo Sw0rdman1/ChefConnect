5fbb0124b23ca1945c0140b59d43ea8d514daf47
Bozidar Vujasinovic <bozidar.vujasinovic@scripttic.com>
2024-07-09 13:34:36 +0200

    Update authentication layout and add login and welcome screens

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -4,9 +4,9 @@ import { Stack } from 'expo-router';
 export default function AuthLayout() {
     return (
         <Stack>
-            <Stack.Screen name="welcome" options={{ headerShown: false }} />
-            <Stack.Screen name="log-in" options={{ headerShown: false }} />
-            <Stack.Screen name="registration" options={{ headerShown: false }} />
+            <Stack.Screen name="index" options={{ headerShown: false, animation: 'fade_from_bottom', gestureEnabled: false }} />
+            <Stack.Screen name="log-in" options={{ headerShown: false, animation: 'fade_from_bottom', gestureEnabled: false }} />
+            <Stack.Screen name="registration" options={{ headerShown: false, animation: 'fade_from_bottom', gestureEnabled: false }} />
         </Stack>
     );
 }
--- app/(auth)/log-in.tsx
+++ app/(auth)/log-in.tsx
@@ -1,14 +1,44 @@
 import { Text, View } from '@/components/ui/Themed'
+import { Image } from 'expo-image'
 import { StyleSheet } from 'react-native'
 
 const LogInScreen = () => {
     return (
-        <View>
-            <Text>LogInScreen</Text>
+        <View style={styles.container}>
+            <View style={styles.imageContainer}>
+                <View style={styles.imageChildContainer}>
+                    <Image
+                        source={require('../../assets/images/auth/login-banner.jpg')}
+                        style={styles.image}
+                    />
+                </View>
+            </View>
         </View>
     )
 }
 
 export default LogInScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    imageContainer: {
+        height: 300,
+        width: '100%',
+        transform: [{ scaleX: 2 }],
+        borderBottomStartRadius: 200,
+        borderBottomEndRadius: 200,
+        overflow: 'hidden',
+    },
+    imageChildContainer: {
+        flex: 1,
+        transform: [{ scaleX: 0.5 }],
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    image: {
+        width: '100%',
+        aspectRatio: 1,
+    },
+})
