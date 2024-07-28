39c4d7ca1066f4fc6e91c32e3b302863f1588e1f
Božidar <vujasinovicb2019@gmail.com>
2024-07-28 15:10:56 +0200

    chore: Refactor SaveButton component and update styles

--- app/(main)/(recipe)/[recipeID].tsx
+++ app/(main)/(recipe)/[recipeID].tsx
@@ -1,56 +1,54 @@
-import LoadingScreen from '@/components/ui/LoadingScreen';
-import { StyleSheet } from 'react-native'
-import { View } from '@/components/ui/Themed'
-import { useColors } from '@/hooks/useColors';
-import { useRecipe } from '@/hooks/useRecipes';
-import { router, useLocalSearchParams } from 'expo-router';
-import AnimatedHeader from '@/components/ui/AnimatedHeader';
-import RecipeHeader from '@/components/main/recipe/RecipeHeader';
-import RecipeTitle from '@/components/main/recipe/RecipeTitle';
-import IngredientsList from '@/components/main/recipe/IngredientsList';
-
+import LoadingScreen from "@/components/ui/LoadingScreen";
+import { StyleSheet } from "react-native";
+import { View } from "@/components/ui/Themed";
+import { useColors } from "@/hooks/useColors";
+import { useRecipe } from "@/hooks/useRecipes";
+import { router, useLocalSearchParams } from "expo-router";
+import AnimatedHeader from "@/components/ui/AnimatedHeader";
+import RecipeHeader from "@/components/main/recipe/RecipeHeader";
+import RecipeTitle from "@/components/main/recipe/RecipeTitle";
+import IngredientsList from "@/components/main/recipe/IngredientsList";
+import SaveButton from "@/components/main/recipe/SaveButton";
 
 const RecipeScreen = () => {
-    const { recipeID } = useLocalSearchParams<{ recipeID: string }>();
-    const { tint, background } = useColors();
-    const { recipe, loading } = useRecipe(recipeID as string);
+  const { recipeID } = useLocalSearchParams<{ recipeID: string }>();
+  const { tint, background } = useColors();
+  const { recipe, loading } = useRecipe(recipeID as string);
 
-    if (loading) {
-        return (
-            <LoadingScreen />
-        )
-    }
+  if (loading) {
+    return <LoadingScreen />;
+  }
 
-    if (!recipe) {
-        router.back();
-        return null;
-    }
+  if (!recipe) {
+    router.back();
+    return null;
+  }
 
+  return (
+    <AnimatedHeader
+      headerComponent={<RecipeHeader recipe={recipe} />}
+      minHeight={300}
+      maxHeight={550}
+    >
+      <View style={[styles.container, { backgroundColor: `${background}95` }]}>
+        <RecipeTitle recipe={recipe} />
+        <SaveButton />
+        <IngredientsList recipe={recipe} />
+      </View>
+    </AnimatedHeader>
+  );
+};
 
-    return (
-        <AnimatedHeader
-            headerComponent={<RecipeHeader recipe={recipe} />}
-            minHeight={300}
-            maxHeight={550}
-        >
-            <View style={[styles.container, { backgroundColor: `${background}95` }]}>
-                <RecipeTitle recipe={recipe} />
-                <IngredientsList recipe={recipe} />
-            </View>
-        </AnimatedHeader>
-    )
-}
-
-export default RecipeScreen
+export default RecipeScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        height: 1500,
-        gap: 10,
-        paddingHorizontal: 15,
-    },
-    text: {
-        fontSize: 32,
-        fontWeight: 'bold'
-    }
-})
+  container: {
+    height: 1500,
+    gap: 10,
+    paddingHorizontal: 15,
+  },
+  text: {
+    fontSize: 32,
+    fontWeight: "bold",
+  },
+});
--- components/main/recipe/RecipeTitle.tsx
+++ components/main/recipe/RecipeTitle.tsx
@@ -1,104 +1,100 @@
-import { Text } from '@/components/ui/Themed';
-import { useColors } from '@/hooks/useColors';
-import Recipe from '@/models/Recipe';
-import { Ionicons } from '@expo/vector-icons';
-import { StyleSheet, View } from 'react-native'
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
-import SaveButton from './SaveButton';
+import { Text } from "@/components/ui/Themed";
+import { useColors } from "@/hooks/useColors";
+import Recipe from "@/models/Recipe";
+import { Ionicons } from "@expo/vector-icons";
+import { StyleSheet, View } from "react-native";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import SaveButton from "./SaveButton";
+import { Image } from "expo-image";
 interface RecipeTitleProps {
-    recipe: Recipe;
+  recipe: Recipe;
 }
 
 const RecipeTitle: React.FC<RecipeTitleProps> = ({ recipe }) => {
-    const { backgroundDarker } = useColors();
-    return (
-        <View style={styles.container}>
-            <View style={styles.nameContainer}>
-                <Text style={styles.title}>
-                    {recipe.title}
-                </Text>
-                <Text style={styles.description}>
-                    {recipe.description}
-                </Text>
-            </View>
-            <SaveButton />
-            <View style={styles.infoContainer}>
-                <View style={[styles.prepTime, { backgroundColor: backgroundDarker }]}>
-                    <View style={styles.label}>
-                        <Text style={styles.labelText}>
-                            Prep Time
-                        </Text>
-                        <Ionicons name="time" size={16} color="gray" />
-                    </View>
-                    <Text style={styles.info}>
-                        {recipe.prepareTime} Minutes
-                    </Text>
-                </View>
-                <View style={[styles.prepTime, { backgroundColor: backgroundDarker }]}>
-                    <View style={styles.label}>
-                        <Text style={styles.labelText}>
-                            Calories
-                        </Text>
-                        <MaterialCommunityIcons name="fire" size={22} color="gray" />
-                    </View>
-                    <Text style={styles.info}>
-                        163 kcal
-                    </Text>
-                </View>
-            </View>
-
+  const { backgroundDarker } = useColors();
+  return (
+    <View style={styles.container}>
+      <View style={styles.nameContainer}>
+        <Text style={styles.title}>{recipe.title}</Text>
+        <Text style={styles.description}>{recipe.description}</Text>
+      </View>
+      <View style={styles.infoContainer}>
+        <View style={styles.prepTime}>
+          <View style={styles.label}>
+            <Image
+              source={require("../../../assets/images/main/clock.png")}
+              style={styles.icon}
+            />
+            <Text style={styles.labelText}>Preparation Time:</Text>
+          </View>
+          <Text style={styles.info}>{recipe.prepareTime} Minutes</Text>
         </View>
-    )
-}
+        <View style={styles.prepTime}>
+          <View style={styles.label}>
+            <Image
+              source={require("../../../assets/images/main/fire.png")}
+              style={styles.icon}
+            />
+            <Text style={styles.labelText}>Calories:</Text>
+          </View>
+          <Text style={styles.info}>163 kcal</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
 
-export default RecipeTitle
+export default RecipeTitle;
 
 const styles = StyleSheet.create({
-    container: {
-        gap: 5,
-    },
-    nameContainer: {
-        borderBottomWidth: 1,
-        borderBottomColor: 'lightgray',
-        paddingBottom: 10,
-    },
-    title: {
-        fontSize: 32,
-        fontWeight: 'bold'
-    },
-    description: {
-        paddingTop: 5,
-        paddingLeft: 2,
-        fontSize: 18,
-        color: 'gray',
-    },
-    infoContainer: {
-        paddingVertical: 15,
-        flexDirection: 'row',
-        justifyContent: 'space-evenly',
-    },
-    prepTime: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        gap: 5,
-        width: 150,
-        height: 70,
-        borderRadius: 10,
-    },
-    label: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    labelText: {
-        fontSize: 15,
-        color: 'gray',
-        fontWeight: 'bold',
-        paddingRight: 2,
-    },
-    info: {
-        fontSize: 16,
-        fontWeight: 'bold'
-    }
+  container: {
+    gap: 5,
+  },
+  nameContainer: {
+    borderBottomWidth: 1,
+    borderBottomColor: "lightgray",
+    paddingBottom: 15,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+  },
+  description: {
+    paddingTop: 5,
+    paddingLeft: 2,
+    fontSize: 18,
+    color: "gray",
+  },
+  infoContainer: {
+    paddingVertical: 15,
+    paddingHorizontal: 5,
+    gap: 15,
+  },
+  prepTime: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 5,
+    borderRadius: 10,
+  },
 
-})
+  label: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 10,
+  },
+  icon: {
+    width: 20,
+    height: 20,
+  },
+  labelText: {
+    fontSize: 18,
+    color: "gray",
+    fontWeight: "bold",
+    paddingRight: 2,
+  },
+  info: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
