85d836d06bf12bf806a95160b445efb7c1b8f475
Bozidar Vujasinovic <bozidar.vujasinovic@scripttic.com>
2024-09-17 09:53:43 +0200

    Remove tint

--- app/(auth)/log-in.tsx
+++ app/(auth)/log-in.tsx
@@ -7,15 +7,12 @@ import Banner from '@/components/auth/Banner';
 import SwitchForm from '@/components/auth/SwitchForm';
 
 const LogInScreen = () => {
-    const { tint } = useColors()
-
     return (
         <View style={styles.container}>
             <Banner source={require('../../assets/images/auth/login-banner.jpg')} height={250} />
             <LogInForm />
             <AppleSignButton isLogin />
             <SwitchForm isLogin />
-
         </View>
     )
 }
